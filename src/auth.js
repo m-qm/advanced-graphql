5fc068f5c123086e6681eeba7aa568738ab8a2e7
m-qm <mireiaquerol@gmail.com>
2020-05-02 00:33:16 +0200

    feat: add auth endpoint

--- src/auth.js
+++ src/auth.js
@@ -4,16 +4,32 @@ const jwt = require ('jsonwebtoken');
 const {models} = require ('./db');
 const secret = 'catpack';
 
+const getUserFromToken = token => {
+  try {
+    const user = jwt.verify (token, secret);
+    return models.User.findOne ({id: user.id});
+  } catch (e) {
+    return null;
+  }
+};
+
 const authenticated = next => (root, args, context, info) => {
   if (!context.user) {
-    throw new Error ('not authorized');
+    throw new AuthenticationError ('must authenticate');
   }
   return root, args, context, info;
 };
 
 const authorized = (role, next) => (root, args, context, info) => {
   if (!context.user.role !== role) {
-    throw new Error ('not authorized');
+    throw new AuthenticationError (`you must have ${role} role`);
   }
   return root, args, context, info;
+};
+
+module.exports = {
+  getUserFromToken,
+  authenticated,
+  authorized,
+  createToken,
 };
